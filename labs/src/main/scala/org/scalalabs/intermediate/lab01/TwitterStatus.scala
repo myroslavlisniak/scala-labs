4e15cb34edd61ae0c9709f99f3f484247425d8cf
Myroslav Lisniak <acnipin@gmail.com>
2014-11-17 22:26:23 +0200

    lab01 intermediate

--- labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterStatus.scala
+++ labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterStatus.scala
@@ -1,2 +1,48 @@
 package org.scalalabs.intermediate.lab01
 
+import java.util.Locale
+
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+
+
+abstract class TwitterStatus {
+
+  val id: Long
+  val text: String
+  val source: String
+  val truncated: Boolean
+  val inReplyToStatusId: Option[Long]
+  val inReplyToUserId: Option[Long]
+  val favorited: Boolean
+  val inReplyToScreenName: Option[String]
+  val user: TwitterUser
+  val createdAt : DateTime
+}
+
+object TwitterStatus {
+  def apply(node: scala.xml.Node): TwitterStatus = {
+    new TwitterStatus {
+      val twitterDateTimeFormat = DateTimeFormat.forPattern("EE MMM dd HH:mm:ss Z yyyy").withLocale(Locale.US)
+      val createdAt = twitterDateTimeFormat.parseDateTime((node \ "created_at").text)
+      val id = (node \ "id").text.toLong
+      val text = (node \ "text").text
+      val source = (node \ "source").text
+      val truncated = (node \ "truncated").text.toBoolean
+      val inReplyToStatusId = {
+        val value = (node \ "in_reply_to_status_id").text
+        if (value == "") None else Some(value.toLong)
+      }
+      val inReplyToUserId = {
+        val value = (node \ "in_reply_to_user_id").text
+        if (value == "") None else Some(value.toLong)
+      }
+      val favorited = (node \ "favorited").text.toBoolean
+      val user = TwitterUser((node \ "user")(0))
+      val inReplyToScreenName = {
+        val value = (node \ "in_reply_to_screen_name").text
+        if (value == "") None else Some(value)
+      }
+    }
+  }
+}
--- labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterUser.scala
+++ labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterUser.scala
@@ -1,3 +1,31 @@
 package org.scalalabs.intermediate.lab01
 
+abstract class TwitterUser{
+  val id: Long
+  val name : String
+  val screen_name : String
+  val description : String
+  val location : String
+  val url : String
+  val profileImageUrl : String
+  val statusesCount : Int
+  val friendsCount : Int
+  val followersCount : Int
+}
 
+object TwitterUser {
+  def apply(node : scala.xml.Node) : TwitterUser = {
+    new TwitterUser {
+      val id = (node \ "id").text.toLong
+      val name = (node \ "name").text
+      val screen_name = (node \ "screen_name").text
+      val description = (node \ "description").text
+      val location = (node \ "location").text
+      val url = (node \ "url").text
+      val profileImageUrl = (node \ "profile_image_url").text
+      val statusesCount = (node \ "statuses_count").text.toInt
+      val friendsCount = (node \ "friends_count").text.toInt
+      val followersCount = (node \ "followers_count").text.toInt
+    }
+  }
+}
